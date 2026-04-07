e9d238b03726404ec53968fce6f838017663e695
Liren Tu <tuliren@gmail.com>
2026-04-07 13:52:51 -0700

    Fix domain

--- apps/web/src/constants.ts
+++ apps/web/src/constants.ts
@@ -4,7 +4,7 @@ export const MINOR_COLOR = 'rgba(255,122,40,0.8)';
 export const TITLE = 'ReadTube';
 export const DESCRIPTION =
   'Turn YouTube subscriptions into a personal newsletter. Reclaim focus in a world engineered for distraction.';
-export const DOMAIN = 'readtube.app';
+export const DOMAIN = 'read.tube';
 export const DOMAIN_URL = `https://${DOMAIN}`;
 export const CONTACT_EMAIL = `contact@${DOMAIN}`;
 
