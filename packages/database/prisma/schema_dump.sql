272cc8cbe5830c96c23ec2963a6af2686a18455d
Liren Tu <tuliren@gmail.com>
2026-04-10 09:12:49 -0700

    Update schema dump

--- packages/database/prisma/schema_dump.sql
+++ packages/database/prisma/schema_dump.sql
@@ -38,6 +38,7 @@ CREATE TABLE "UserSubscription" (
   "user_id" TEXT NOT NULL,
   "channel_id" TEXT NOT NULL,
   "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "read_at" TIMESTAMP(3),
   CONSTRAINT "UserSubscription_pkey" PRIMARY KEY ("id")
 );
 
