2286dc9853111dd55d39ce66dd247b587d6e6ed2
Liren Tu <tuliren@gmail.com>
2026-04-17 08:14:35 -0700

    Hide pricing table for now

--- apps/web/src/app/(marketing)/page.tsx
+++ apps/web/src/app/(marketing)/page.tsx
@@ -1,15 +1,14 @@
 import CallToAction from '@/components/CallToAction';
 import Features from '@/components/Features';
 import Hero from '@/components/Hero';
-import Pricing from '@/components/Pricing';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Features />
-      <Pricing />
-      <CallToAction />
+      {/*<Pricing />*/}
+      <CallToAction theme="light" />
     </>
   );
 }
